3c8fff381144b6c0ce0bdd4c2fd8655e5d87d3ef
JossePerez <jose.perez18@tectijuana.edu.mx>
2023-08-18 15:24:45 -0700

    Creacion del modulo foro

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -140,6 +140,44 @@
         </div>
 
     </article>
+
+    <article class="container text-center">
+        <div class="row">
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-foro" >
+                    <div class="div-foro-circular">
+                        <!-- Cargar un img -->
+                    </div>
+                    <div class="div-foro-info">
+                        <h3>Titulo</h3>
+                        <p>Contenido</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-foro" >
+                    <div class="div-foro-circular">
+                        <!-- Cargar un img -->
+                    </div>
+                    <div class="div-foro-info">
+                        <h3>Titulo</h3>
+                        <p>Contenido</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-foro" >
+                    <div class="div-foro-circular">
+                        <!-- Cargar un img -->
+                    </div>
+                    <div class="div-foro-info">
+                        <h3>Titulo</h3>
+                        <p>Contenido</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
 </template>
 
 <script>
@@ -168,5 +206,24 @@ import 'bootstrap/dist/js/bootstrap.js';
 .div-image-card{
     border-radius: 15px;
     margin: 10px 10px 0 10px!important;
+}
+
+
+.div-foro{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.div-foro-circular{
+    width: 10rem;
+    height: 10rem;
+    border-radius: 100%;
+    background-color: rgb(16, 86, 218);
+}
+
+.div-foro-info{
+    color: #202020;
 }
 </style>
