b4e4d063da0bf878f7f622f2bd7afd7101d8e2b0
Jose de Jesus Perez Padilla <jose.perez18@tectijuana.edu.mx>
2023-08-17 17:14:27 -0700

    Se agrega el proyecto

---
+++ src/components/Principal.vue
@@ -0,0 +1,172 @@
+<template>
+    <!--Aqui va el componente principal-->
+    <article class="container text-center">
+        <div class="row">
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-card">
+                    <div class="card-main">
+                       <div style="background-color: blue;" class="div-image-card">
+                           <img src="../assets/logo.png" alt="" class="img-fluid">
+                       </div>
+                       <div>
+                           <br>
+                           <h3>Hola</h3>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, asperiores?</p>
+                       </div>
+                    </div>
+                    <div class="div-card-bottom">
+                        <div>
+                            <p>Izquierda</p>
+                        </div>
+                        <div>
+                            <p>Derecha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-card">
+                    <div class="card-main">
+                       <div style="background-color: blue;" class="div-image-card">
+                           <img src="../assets/logo.png" alt="" class="img-fluid">
+                       </div>
+                       <div>
+                           <br>
+                           <h3>Hola</h3>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, asperiores?</p>
+                       </div>
+                    </div>
+                    <div class="div-card-bottom">
+                        <div>
+                            <p>Izquierda</p>
+                        </div>
+                        <div>
+                            <p>Derecha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-card">
+                    <div class="card-main">
+                       <div style="background-color: blue;" class="div-image-card">
+                           <img src="../assets/logo.png" alt="" class="img-fluid">
+                       </div>
+                       <div>
+                           <br>
+                           <h3>Hola</h3>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, asperiores?</p>
+                       </div>
+                    </div>
+                    <div class="div-card-bottom">
+                        <div>
+                            <p>Izquierda</p>
+                        </div>
+                        <div>
+                            <p>Derecha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-card">
+                    <div class="card-main">
+                       <div style="background-color: blue;" class="div-image-card">
+                           <img src="../assets/logo.png" alt="" class="img-fluid">
+                       </div>
+                       <div>
+                           <br>
+                           <h3>Hola</h3>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, asperiores?</p>
+                       </div>
+                    </div>
+                    <div class="div-card-bottom">
+                        <div>
+                            <p>Izquierda</p>
+                        </div>
+                        <div>
+                            <p>Derecha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-card">
+                    <div class="card-main">
+                       <div style="background-color: blue;" class="div-image-card">
+                           <img src="../assets/logo.png" alt="" class="img-fluid">
+                       </div>
+                       <div>
+                           <br>
+                           <h3>Hola</h3>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, asperiores?</p>
+                       </div>
+                    </div>
+                    <div class="div-card-bottom">
+                        <div>
+                            <p>Izquierda</p>
+                        </div>
+                        <div>
+                            <p>Derecha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-ms-12 col-md-6 col-lg-4 col-xl-4">
+                <div class="div-card">
+                    <div class="card-main">
+                       <div style="background-color: blue;" class="div-image-card">
+                           <img src="../assets/logo.png" alt="" class="img-fluid">
+                       </div>
+                       <div>
+                           <br>
+                           <h3>Hola</h3>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, asperiores?</p>
+                       </div>
+                    </div>
+                    <div class="div-card-bottom">
+                        <div>
+                            <p>Izquierda</p>
+                        </div>
+                        <div>
+                            <p>Derecha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </article>
+</template>
+
+<script>
+export default{
+
+}
+import 'bootstrap/dist/js/bootstrap.js';
+</script>
+
+<style>
+/* Importacion de Bootstrap */ 
+.div-card{  
+    border-radius: 15px;
+    border: 2px solid #5E6A73;
+    padding: 0px;
+    box-shadow: 0px 10px 15px #000000;
+    margin-bottom: 35px;
+}
+.div-card-bottom{
+    border-top: 2px solid #5E6A73;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 10px 0 10px;
+}
+
+.div-image-card{
+    border-radius: 15px;
+    margin: 10px 10px 0 10px!important;
+}
+</style>
